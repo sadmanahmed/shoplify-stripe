2659ceffb7af71b367360c6e3e638503aa752588
sadmanahmed <9040618-sadmanahmed@users.noreply.gitlab.com>
2021-07-15 05:23:54 +0600

    fix

--- db/migrate/20210714222116_add_sales_count_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddSalesCountToProducts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :products, :sales_count, :integer, null: false, default: 0
-  end
-end
--- db/migrate/20210714230130_remove_sales.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSales < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :products, :sales_count
-  end
-end
